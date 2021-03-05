20ec2ec2a4d290fb89e2ae1bbd16d8ddf704e594
Ty D'Angelo <tydangelo18@gmail.com>
2021-03-05 15:41:38 -0600

    implemented auth middleware to authenticate protected routes.

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+// Bring in JWT
+const jwt = require(`jsonwebtoken`);
+// Bring in config
+const config = require(`config`);
+
+// Export a middleware function for implementing into a protected route
+module.exports = function (req, res, next) {
+  // Get Token from the header
+  const token = req.header(`x-auth-token`);
+
+  // Check if the user has no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token: authorization denied.' });
+  }
+
+  // Verify the token if one exists
+  try {
+    const decoded = jwt.verify(token, config.get(`jwtSecret`));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
