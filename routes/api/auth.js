8bc392ffcfeac6bcc7ecb137e0ae2bacdf1fd917
Ty D'Angelo <tydangelo18@gmail.com>
2021-03-06 10:04:32 -0600

    implemented auth middleware into a protected route.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,14 +3,27 @@
 const express = require(`express`);
 /// Bring in Express router
 const router = express.Router();
+// Bring in Auth Middleware
+const auth = require('../../middleware/auth');
+// Bring in User Model
+const User = require('../../models/User');
 
 /// Routes
 
-//// TEST ROUTE
 // 1) @route GET api/users
-// 2) @desc Test Route
+// 2) @desc Get a User
 // 3) @access Public (doesn't need authentication to view)
-router.get(`/`, (req, res) => res.send(`Auth Route`));
+router.get(`/`, auth, async (req, res) => {
+  try {
+    // Make a call to the Database to the specific user trying to send a request
+    // .select('-password') leaves off the password in the data
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(`Server Error`);
+  }
+});
 
 // Export Routes
 module.exports = router;
