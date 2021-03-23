27952eee7f2fe79097fe5872bb402c43ca4e3ab9
Ty D'Angelo <tydangelo18@gmail.com>
2021-03-23 15:40:57 -0500

    Changed repo name, re-initaialized npm and re-tested auth and user ro

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,6 +7,13 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 // Bring in User Model
 const User = require('../../models/User');
+// Bring in JSONWEBTOKEN
+const jwt = require('jsonwebtoken');
+// Bring in bcrypt
+const bcrypt = require('bcryptjs');
+// Bring in Express Validator
+const { check, validationResult } = require(`express-validator`);
+const config = require('config');
 
 /// Routes
 
@@ -24,6 +31,69 @@ router.get(`/`, auth, async (req, res) => {
     res.status(500).send(`Server Error`);
   }
 });
+
+// Login ROUTE
+// 1) @route POST api/auth
+// 2) @desc Authenticate a user and get token
+// 3) @access Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    // If input from the client side is invalid based on the above criteria, a response will be sent to client side
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure the req.body for easier access below
+    const { email, password } = req.body;
+
+    // Create a query with Mongoose to find a User
+    try {
+      let user = await User.findOne({ email });
+      // Checks if user does not exist
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials ' }] });
+      }
+
+      // Compare plain text password from client side and compare it to the encrypted password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials ' }] });
+      }
+
+      // Return JWT
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get(`jwtSecret`),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      // Send to the client side
+      res.status(500).send(`Server Error`);
+    }
+  }
+);
 
 // Export Routes
 module.exports = router;
