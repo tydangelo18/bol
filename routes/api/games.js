efe410d8084a1724e360dc6182e01e72e4fe86e0
Ty D'Angelo <tydangelo18@gmail.com>
2021-04-02 19:49:33 -0500

    Created get route route to get a game by id.

--- routes/api/games.js
+++ routes/api/games.js
@@ -56,5 +56,25 @@ router.get(`/`, auth, async (req, res) => {
   }
 });
 
+// @route GET api/games/:id
+// @desc Get game by id
+// @access Private
+router.get(`/:id`, auth, async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.id);
+    // Check if no game exists
+    if (!game) {
+      return res.status(404).json({ msg: `Game not found` });
+    }
+    res.json(game);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === `ObjectId`) {
+      return res.status(404).json({ msg: `Game not found` });
+    }
+    res.status(500).send(`Server Error`);
+  }
+});
+
 // Export Route for External Use
 module.exports = router;
