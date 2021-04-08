a9d172a549dfaa5d47e79ead6f8e19ea296a8442
Ty D'Angelo <tydangelo18@gmail.com>
2021-04-08 13:17:22 -0500

    Import Register component to main component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment className='App'>
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment className='App'>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 };
 
