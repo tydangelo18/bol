9aaa39bef079a65664b8af10a9dde24d8cb7abe0
Ty D'Angelo <tydangelo18@gmail.com>
2021-04-16 22:55:56 -0500

    Created Navbar and Dashboard Components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 import Alert from './components/layout/Alert';
 import './App.css';
 // Connects Redux
@@ -26,12 +28,14 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
+          <Navbar />
           <Route exact path='/' component={Landing} />
           <section className='container'>
             <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
