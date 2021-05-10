4c23edd59ddb621b6af010ece45f21850fac1964
Ty D'Angelo <tydangelo18@gmail.com>
2021-05-09 21:33:20 -0500

    Mapped Frame Metrics to Metrics component so that user can see all frame metrics together in one page.

--- client/src/components/metrics/Metrics.js
+++ client/src/components/metrics/Metrics.js
@@ -1,18 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import SingleGameMetrics from './SingleGameMetrics';
+import { getGames } from '../../actions/game';
 
-const Metrics = ({ auth }) => {
+const Metrics = ({ auth, getGames, game: { games } }) => {
+  useEffect(() => {
+    getGames();
+  }, [getGames]);
   return (
     auth.isAuthenticated &&
     auth.loading === false && (
       <Fragment>
         <div>
           <Link to='/games'>Back to all games</Link>
-          <h4>Metrics Page</h4>
+          <h4>Metrics</h4>
           <h6>Graph will display here</h6>
-          <h6>Metrics Figures will display below</h6>
+          <div>
+            {games.map((game) => (
+              <SingleGameMetrics key={game._id} game={game} />
+            ))}
+          </div>
         </div>
       </Fragment>
     )
@@ -21,10 +30,13 @@ const Metrics = ({ auth }) => {
 
 Metrics.propTypes = {
   auth: PropTypes.object.isRequired,
+  getGames: PropTypes.func.isRequired,
+  game: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
+  game: state.game,
 });
 
-export default connect(mapStateToProps, {})(Metrics);
+export default connect(mapStateToProps, { getGames })(Metrics);
