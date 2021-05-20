dbd1be4d98169e84fe54ae02665b6ca7e9785c1d
Ty D'Angelo <tydangelo18@gmail.com>
2021-05-20 12:16:04 -0500

    Ready for deployment.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require(`express`);
 /// Bring in MongoDB Connection
 const connectDB = require('./config/db');
+// Production
+const path = require(`path`);
 
 /// Initialize app variable w/ Express
 const app = express();
@@ -20,9 +22,21 @@ app.use(`/api/auth`, require(`./routes/api/auth`));
 app.use(`/api/profile`, require(`./routes/api/profile`));
 app.use(`/api/games`, require(`./routes/api/games`));
 
+// Production
+// Serve Static Assets in Production
+// Check for Production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname.anchor, 'client', 'build', 'index.html')
+    );
+  });
+}
+
 /// Take app variable and listen on port 5000
 //// ---> Will look for an env variable called PORT so when deployed it will look for that PORT
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
-
